5b3b1cda13ab55a7cd26aebe208711c071c58969
suarezmotta <116030678+suarezmotta@users.noreply.github.com>
2022-10-17 20:40:05 -0500

    Update Entrega1_DTS16QAM.m

--- E1/Entrega1_DTS16QAM.m
+++ E1/Entrega1_DTS16QAM.m
@@ -10,15 +10,13 @@
 %------------------------------INICIO--------------------------------
 clear;
 tic;
-Ro=0.35 ; U=16; L=8; Tf=1;
-h1 = rcosfir(Ro, L, U, Tf,'sqrt') ; %Funcion de transferencia del filtro conformador de pulsos
+for ind=1:1:100
 %-------------------CONSTRUCCION DE MENSAJE------------------------
-Ns=1000000; %número de bits
+Ns=100000; %número de bits
 M=16; %orden de la modulación
 Es=1; %este valor depende de la constelación
 %vector de símbolos que entra al filtro conformador
 msg=randsrc(1,Ns,[0 1]); %genera los bits a transmitir
-for ind=1:1:32 %Ciclo iterativo para construir grafica de rendimiento
 %--------CONSTRUCCION DE LOS SIMBOLOS------------
 for i=1:4:length(msg)
     if msg(i)==0 && msg(i+1)==0 && msg(i+2)==0 && msg(i+3)==0
@@ -56,10 +54,49 @@ for i=1:4:length(msg)
     end
 end
 %-----------------MODULACION QAM----------------------
-moduladaqam=qammod(simboloqam,16);
+moduladaqam=[];
+for i=1:1:length(simboloqam)
+    switch simboloqam(i)
+        case 0
+            moduladaqam(i)=-3+3i;
+        case 1
+            moduladaqam(i)=-3+1i;
+        case 2
+            moduladaqam(i)=-3-3i;
+        case 3
+            moduladaqam(i)=-3-1i;
+        case 4
+            moduladaqam(i)=-1+3i;
+        case 5
+            moduladaqam(i)=-1+1i;
+        case 6
+            moduladaqam(i)=-1-3i;
+        case 7
+            moduladaqam(i)=-1-1i;
+        case 8
+            moduladaqam(i)=3+3i;
+        case 9
+            moduladaqam(i)=3+1i;
+        case 10
+            moduladaqam(i)=3-3i;
+        case 11
+            moduladaqam(i)=3-1i;
+        case 12
+            moduladaqam(i)=1+3i;
+        case 13
+            moduladaqam(i)=1+1i;
+        case 14
+            moduladaqam(i)=1-3i;
+        case 15
+            moduladaqam(i)=1-1i;
+    end
+end
 s=moduladaqam;
 %-----------------------------------------------------
 %-----------FILTRO CONFORMADOR DE PULSO---------------
+Ro=0.35 ; U=16; L=8; Tf=1;
+h1 = rcosfir(Ro, L, U, Tf,'sqrt') ; %Funcion de transferencia del filtro
+
 % upsample 
 su1=upsample(s,U);
 %Agregacion de ceros al final del vector para evitar recorte por transiente
@@ -74,7 +111,7 @@ ebno=1*ind; %EbNo en veces
 Es=10; %Energia promedio de la constelacion
 varianza=Es/(2*log2(M)*ebno);%determina la varianza de ruido
 %Z=sigma*randn(1,length(S1)); %forma para dimensionar correctamente el ruido que se introduce al sistema 
-ruidoqam=moduladaqam+((sqrt(varianza))*(randn(1,length(moduladaqam))+j*randn(1,length(moduladaqam))));
+ruidoqam=moduladaqam+((sqrt(varianza/2))*(randn(1,length(moduladaqam))+j*randn(1,length(moduladaqam))));
 %snr=1.9;%Cambia la snr de la señal y el ruido (es funcion de la varianza)
 %ruidoqam=awgn(moduladaqam,snr);
 x1=ruidoqam;
@@ -89,49 +126,46 @@ ruidoqam=s1;
 %figure,
 %plot(x1)
 %-----------------------------------------------------
-%------------RECONSTRUCCION DEL SIMBOLO---------------
-demodqam=qamdemod(ruidoqam,16);
-%-----------------------------------------------------
 %-----------DEMODULACION QAM--------------
 %demodqam=qamdemod(ruidoqam,16);%Desicion QAM
-senalentrada=ruidoqam;
-for i=1:1:length(senalentrada)
-    if real(senalentrada(i))<=-2
-        if imag(senalentrada(i))<=-2
+demodqam=ruidoqam;
+for i=1:1:length(demodqam)
+    if real(demodqam(i))<=-2
+        if imag(demodqam(i))<=-2
             funsimb(i)=2;
-        elseif imag(senalentrada(i))<=0
+        elseif imag(demodqam(i))<=0
             funsimb(i)=3;
-        elseif imag(senalentrada(i))<=2
+        elseif imag(demodqam(i))<=2
             funsimb(i)=1;
         else
             funsimb(i)=0;
         end
-    elseif real(senalentrada(i))<=0
-        if imag(senalentrada(i))<=-2
+    elseif real(demodqam(i))<=0
+        if imag(demodqam(i))<=-2
             funsimb(i)=6;
-        elseif imag(senalentrada(i))<=0
+        elseif imag(demodqam(i))<=0
             funsimb(i)=7;
-        elseif imag(senalentrada(i))<=2
+        elseif imag(demodqam(i))<=2
             funsimb(i)=5;
         else
             funsimb(i)=4;
         end
-    elseif real(senalentrada(i))<=2
-        if imag(senalentrada(i))<=-2
+    elseif real(demodqam(i))<=2
+        if imag(demodqam(i))<=-2
             funsimb(i)=14;
-        elseif imag(senalentrada(i))<=0
+        elseif imag(demodqam(i))<=0
             funsimb(i)=15;
-        elseif imag(senalentrada(i))<=2
+        elseif imag(demodqam(i))<=2
             funsimb(i)=13;
         else
             funsimb(i)=12;
         end
     else
-        if imag(senalentrada(i))<=-2
+        if imag(demodqam(i))<=-2
             funsimb(i)=10;
-        elseif imag(senalentrada(i))<=0
+        elseif imag(demodqam(i))<=0
             funsimb(i)=11;
-        elseif imag(senalentrada(i))<=2
+        elseif imag(demodqam(i))<=2
             funsimb(i)=9;
         else
             funsimb(i)=8;
@@ -224,24 +258,50 @@ for i=1:1:length(demodqam)%Decodificacion Simbolos
         bitqam(4*i)=1;
     end
 end
-%----------------DETECCION DE ERRORES DE TRANSMISION---------------------
+
+
+
+%bitqam(226)=[];%Remuevo el cero agregado para evitar considerar el ultimo digito como ruido o error
+%bitqam=reshape(bitqam,[15 15]);%Ordeno el vector recibido (Ajustado para un mensaje de 15 caracteres)
+%--------DECODIFICACION METODO BCH GF(2)--------------
+%bitqamgf=gf(bitqam);
+%[dec, errordec]=bchdec(bitqamgf,15,7);
+%dec=dec.x %Extraigo la matriz mensaje del campo de Galois;
+%--------DECODIFICACION METODO BCH PRIMITIVO (CICLICO)--------------
+%tablasindrome=syndtable(matparidadbch); %Tabla de sindromes
+%CIdec=decode(bitqam,15,7,'cyclic/binary',genpoly,tablasindrome); %Decodificacion
+%--------RECONSTRUCCION DE MENSAJE-------
+%dec=bi2de(CIdec,'left-msb');
+%msgsalida=num2str(dec','%s')
+%toc
+%------------------GRAFICA DE ANALISIS---------------------
+% tic;
+% simboloerrado=0;
+% for i=1:1:length(demodqam)
+%     if demodqam(i)==simboloqam(i)
+%         plot(real(ruidoqam(i)),imag(ruidoqam(i)),'b.');
+%         hold on
+%     else
+%         plot(real(ruidoqam(i)),imag(ruidoqam(i)),'r*');
+%         hold on
+%         simboloerrado=simboloerrado+1;
+%     end
+% end
+% hold off
+% xlim([-5 5]),ylim([-5 5]),
+% xlabel 'Fase', ylabel Cuadratura; %Graficacion de Resultados
+% title('\bf Diagrama de Constelacion de la Transmision en 16-QAM');
+% toc
+%----------------DISTANCIA HAMMING CODIGO BCH---------------------
+%tic;
 ser=nnz(~(demodqam == simboloqam));
 vser(ind)=ser;
-teober(ind)=((3/8)*erfc(sqrt((4/10)*ebno)));
-ebnodb(ind)=10*log10(ebno);
+ebnodb(ind)=10*log(ebno);
 end
-tiemposimulacion=toc
-vber=vser./(Ns);%BER(casi igual a)SER debido a codificacion gray
-%------------------GRAFICA DE RENDIMIENTO---------------------
-semilogy(ebnodb,vber)
-close all; figure
-semilogy(ebnodb,teober,'bs-','LineWidth',1);
-hold on
-semilogy(ebnodb,vber,'mx-','LineWidth',1);
-axis([0 15 10^-6 1])
-grid off
-legend('Teorica', 'Simulada');
-xlabel('Eb/No, dB')
-ylabel('Tasa de error de BIT')
-title('Curvas de rendimiento para el DTS asignado')
-%------------------------------FIN--------------------------------
+
+
+t=toc
+vser=vser./(Ns.*log2(M))
+semilogy(ebnodb,vser)
+
+% Grafica de simbolos transmitidos (con ruido)
