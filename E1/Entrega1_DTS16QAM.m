e983ed05d9e0f8df21e3a80f36de09d519ea15fc
Nicolas Zambrano <94875666+jnzambranob@users.noreply.github.com>
2022-10-28 06:15:30 -0500

    Entrega 1 - Final
    DTS Basico 16QAM - Coseno alsado - Modulador - Ruido

--- E1/Entrega1_DTS16QAM.m
+++ E1/Entrega1_DTS16QAM.m
@@ -10,15 +10,18 @@
 %------------------------------INICIO--------------------------------
 clear;
 tic;
-Ro=0.35 ; U=16; L=8; Tf=1;
+Ro=0.35 ; U=4; L=8; Tf=1;
 h1 = rcosfir(Ro, L, U, Tf,'sqrt') ; %Funcion de transferencia del filtro conformador de pulsos
 %-------------------CONSTRUCCION DE MENSAJE------------------------
-Nb=1000000; %número de bits
+Nb=2000000; %número de bits
 M=16; %orden de la modulación
 Es=10; %Energia promedio de la constelacion
+Ns=Nb/log2(M);
+R=4
 %vector de símbolos que entra al filtro conformador
 msg=randsrc(1,Nb,[0 1]); %genera los bits a transmitir
 for ind=1:1:32 %Ciclo iterativo para construir grafica de rendimiento
+%---------------------CDMA-----------------------
 %--------CONSTRUCCION DE LOS SIMBOLOS------------
 for i=1:4:length(msg)
     if msg(i)==0 && msg(i+1)==0 && msg(i+2)==0 && msg(i+3)==0
@@ -66,29 +69,34 @@ su1=upsample(s,U);
 su=[su1 zeros(1,2*L*U)];
 %Filtro de transmision
 x1=filter(h1,1,su);
-moduladaqam=x1;
+%-----------------------------------------------------
+%---------------------MODULACION EN PORTADORA---------------------------
+fs = U*R;
+T = 1/R;
+ts = 1/fs;
+fc=(1/4)*(fs); %Para cumplir nyquist
+t = 0 : ts : (Ns*T-ts)+R;
+moduladaqam = sqrt(2)*(real(x1).*cos(2*pi*fc.*t)-imag(x1).*sin(2*pi*fc.*t)); % Traslacion en frecuencia
 %-----------------------------------------------------
 %---------------------RUIDO---------------------------
 %varianza=0;
 ebno=1*ind; %EbNo en veces
 varianza=Es/(2*log2(M)*ebno);%determina la varianza de ruido
 %Z=sigma*randn(1,length(S1)); %forma para dimensionar correctamente el ruido que se introduce al sistema 
-ruidoqam=moduladaqam+((sqrt(varianza))*(randn(1,length(moduladaqam))+j*randn(1,length(moduladaqam))));
+ruidoqam=moduladaqam+sqrt(varianza)*randn(1,length(moduladaqam));
 %snr=1.9;%Cambia la snr de la señal y el ruido (es funcion de la varianza)
 %ruidoqam=awgn(moduladaqam,snr);
 x1=ruidoqam;
-ap=x1;
+x=x1;
 %----------------------------------------
-%---------------------MODULACION EN PORTADORA---------------------------
-% fc=10000;
-% fs=100000;
-% [st,tiempo]=modulate(real(x1),fc,fs,'qam',imag(x1));
-%%plot(st)
-%%----------------------------------------
-%%---------------------DEMODULACION EN PORTADORA---------------------------
-% [srr,sri]=demod(st,fc,fs,'qam');
-% x1=srr+j*sri;
-%%plot(sr)
+%-----------DEMODULACION EN PORTADORA----------------
+Ts = 1/fs;
+Y=x;
+%t = 0 : Ts : Ts*(Ns-1);  
+Y_real = sqrt(2)*Y.*cos(2*pi*fc.*t);    
+Y_imag = -sqrt(2)*Y.*sin(2*pi*fc.*t);
+y = (Y_real + j*Y_imag);
+x1=y;
 %----------------------------------------
 %------------FILTRO CONFORMADOR DE PULSO (FILTRO ACOPLADO)---------------
 %Filtro de recepcion
@@ -239,7 +247,6 @@ teober(ind)=((3/8)*erfc(sqrt((4/10)*ebno)));
 ebnodb(ind)=10*log10(ebno);
 end
 tiemposimulacion=toc
-Ns=Nb/log2(M);
 vber=(vser./(Ns))*(1/log2(M));%BER(casi igual a)SER debido a codificacion gray
 %-----------------------GRAFICA DE RENDIMIENTO-----------------------
 semilogy(ebnodb,vber)
