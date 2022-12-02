95c8991d5801cf0a0d4623d4184395b98a00ec6c
Nicolas Zambrano <94875666+jnzambranob@users.noreply.github.com>
2022-12-02 10:58:57 -0500

    Interferencia banda estrecha
    Update 2

--- E4/Entrega4_MT_INTERFERENCIA.m
+++ E4/Entrega4_MT_INTERFERENCIA.m
@@ -13,12 +13,12 @@ tic;
 Ro=0.35 ; U=4; L=8; Tf=1;
 h1 = rcosfir(Ro, L, U, Tf,'sqrt') ; %Funcion de transferencia del filtro conformador de pulsos
 %-------------------CONSTRUCCION DE MENSAJE------------------------
-Nb=100000; %número de bits
+Nb=1000000; %número de bits
 M=2; %orden de la modulación
-Es=8*8; %Energia promedio de la constelacion
+Es=1; %Energia promedio de la constelacion
 wal=8;
 Ns=Nb/log2(M);
-R=4
+R=4;
 %vector de símbolos que entra al filtro conformador
 msg=randsrc(1,Nb,[0 1]); %genera los bits a transmitir
 for ind=1:1:11 %Ciclo iterativo para construir grafica de rendimiento
@@ -27,11 +27,11 @@ for ind=1:1:11 %Ciclo iterativo para construir grafica de rendimiento
 simbolopsk=msg;
 %-----------------MODULACION QAM----------------------
 moduladapsk=pskmod(simbolopsk,M);
-so=real(moduladapsk);
+s=real(moduladapsk);
 %-----------------------------------------------------
 %---------------------CDMA-----------------------
 %%Codificacion de vectores CDMA
-s=cdmamod(so);
+%s=cdmamod(s);
 %tests=cdmademod(s);
 %-----------------------------------------------------
 %-----------FILTRO CONFORMADOR DE PULSO---------------
@@ -48,7 +48,7 @@ fs = U*R;
 T = 1/R;
 ts = 1/fs;
 fc=(1/4)*(fs); %Para cumplir nyquist
-t = 0 : ts : (Ns*T-ts)+R;
+t = 0 : ts : (Ns*T-ts)+(16/R);
 tc = t./wal;
 portadorapsk = sqrt(2)*(x1.*cos(2*pi*fc.*t)-imag(x1).*sin(2*pi*fc.*t)); % Traslacion en frecuencia
 ppsk = portadorapsk;
@@ -73,7 +73,14 @@ xc=x1;
 %x1bkp2=x;
 %-----------------------------------------------------
 %---------------INTERFERENCIA DE BANDA ESTRECHA----------------------
-x
+%xinterf=randsrc(1,size(x,2),[min(x) max(x)]);
+%xinterf=min(x)+(max(x)-min(x)).*randn(size(x,2),1);
+%xinterf=randn(size(x),'like',x);
+xinterf=randi([ceil(min(x)) ceil(max(x))],1,size(x,2));
+%hint=rcosfir(Ro,2,16,Tf,'sqrt');
+hint=fir1(256,[0.7 0.7001]);
+xinterf=filter(hint,2,xinterf);
+x=x+xinterf;
 %-----------------------------------------------------
 %-------------------DEMODULACION EN PORTADORA-----------------------
 Ts = 1/fs;
@@ -97,7 +104,7 @@ ruidopsk=s1;
 %-----------------------------------------------------
 %------------------DECODIFICACION CDMA-----------------------
 %%Deodificacion de vectores CDMA
-ruidopsk=cdmademod(ruidopsk);
+%ruidopsk=cdmademod(ruidopsk);
 %-----------------------------------------------------
 %-----------DEMODULACION QAM--------------
 %demodqam=qamdemod(ruidoqam,16);%Desicion QAM
@@ -122,8 +129,9 @@ end
 %------------------DETECCION DE ERRORES DE TRANSMISION----------------------
 ser=nnz(~(bitpsk == simbolopsk));
 vser(ind)=ser;
-%teober(ind)=((3/8)*erfc(sqrt((4/10)*ebno)));
-teober(ind)=qfunc(sqrt(0.25.*ebno));
+%teober(ind)=((3/8)*erfc(sqrt((4/10)*ebno)));%BER TEORICA 16QAM
+teober(ind)=((1/2)*erfc(sqrt(ebno)));%BER TEORICA BPSK
+%teober(ind)=qfunc(sqrt(0.25.*ebno));%BER TEORICA CDMA
 ebnodb(ind)=10*log10(ebno);
 end
 tiemposimulacion=toc
@@ -142,5 +150,17 @@ ylabel('Tasa de error de BIT')
 title('Curvas de rendimiento para el DTS asignado')
 %--------------------------------FIN----------------------------------
 %-----------------------GRAFICA DE ESPECTRO-----------------------
-
+%x=xinterf;
+figure
+espectrox =(abs(fftshift(fft(x)))/length(x));   %FFT
+espectroxint =(abs(fftshift(fft(xinterf)))/length(xinterf));   %FFT
+precision = fs/length(x);
+f = linspace(-fs/2+precision/2, fs/2-precision/2, length(x)); %Centrar frecuencia
+plot(f,espectrox);
+hold on
+plot(f,espectroxint,'m-','LineWidth',1);
+axis([0 R*2 0 2*10^-2])
+xlabel('Frecuencia')
+ylabel('Densidad espectral de potencia')
+title('Espectro de señal en el canal')
 %--------------------------------FIN----------------------------------
